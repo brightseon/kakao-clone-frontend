5fc128ed639dbd2bbb06e5de8571daf1cd4e65d3
seon <seonu0823@gmail.com>
2019-11-13 22:50:48 +0900

    회원가입 추가

--- src/Routes/Join.tsx
+++ src/Routes/Join.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState, KeyboardEventHandler, KeyboardEvent } from 'react';
 import styled from 'styled-components';
 import useInput from '../Hooks/useInput';
+import axios from 'axios';
+import { URL } from '../constants';
 
 const Container = styled.div`
     width: 100%;
@@ -66,28 +68,73 @@ const Join = () => {
     const name = useInput('');
     const password = useInput('');
     const passwordConfirm = useInput('');
+    const [error, setError] = useState('');
+
+    const isActive = () => {
+        return (
+            email.value.length > 0 &&
+            name.value.length > 0 &&
+            password.value.length > 0 &&
+            passwordConfirm.value.length > 0
+        );
+    };
+
+    const resetError : KeyboardEventHandler = (e : KeyboardEvent<HTMLInputElement>) => {
+        if(error.length > 0) {
+            setError('');
+        }
+
+        if(e.keyCode === 13) {
+            join();
+        }
+    };
+
+    const validateCheck = () : boolean => {
+        let success = true;
+
+        if(password.value !== passwordConfirm.value) {
+            success = false;
+        }
+
+        return success;
+    };
+
+    const join = async () => {
+        if(!isActive()) return;
+        if(!validateCheck()) {
+            setError('비밀번호와 비밀번호 확인이 맞지 않습니다.');
+
+            return;
+        }
+
+        try {
+            const result = await axios.post(`${ URL }/auth/join`, {
+                email : email.value,
+                name : name.value,
+                password : password.value,
+                password_confirm : passwordConfirm.value
+            });
+    
+            console.log('result : ', result);
+        } catch(err) {
+            console.log('join error : ', err);
+        }
+    };
 
     return (
         <Container>
             <JoinForm>
-                <InputBox>
-                    <Input type="text" placeholder="이메일" value={ email.value } onChange={ email.onChange } />
-                    <Error></Error>
-                </InputBox>
-                <InputBox>
-                    <Input type="text" placeholder="이름" value={ name.value } onChange={ name.onChange } />
-                    <Error></Error>
-                </InputBox>
-                <InputBox>
-                    <Input type="text" placeholder="비밀번호" value={ password.value } onChange={ password.onChange } />
-                    <Error></Error>
-                </InputBox>
-                <InputBox>
-                    <Input type="text" placeholder="비밀번호 확인" value={ passwordConfirm.value } onChange={ passwordConfirm.onChange } />
-                    <Error></Error>
-                </InputBox>
+                <Input type="text" placeholder="이메일" value={ email.value } onChange={ email.onChange }
+                    onKeyDown={ resetError } />
+                <Input type="text" placeholder="이름" value={ name.value } onChange={ name.onChange }
+                    onKeyDown={ resetError } />
+                <Input type="password" placeholder="비밀번호" value={ password.value } 
+                    onChange={ password.onChange } onKeyDown={ resetError } />
+                <Input type="password" placeholder="비밀번호 확인" value={ passwordConfirm.value } 
+                    onChange={ passwordConfirm.onChange } onKeyDown={ resetError } />
+                <Error>{ error }</Error>
             </JoinForm>
-            <JoinButton type="button" active={ false }>회원가입</JoinButton>
+            <JoinButton type="button" active={ isActive() } onClick={ join }>회원가입</JoinButton>
         </Container>
     );
 };
